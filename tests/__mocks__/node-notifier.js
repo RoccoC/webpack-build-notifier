3044ecad82cc7c6027658177d35d1ac5282366e6
RoccoC <roccocataldo@gmail.com>
2019-08-14 17:20:56 -0700

    Committing missing files.

---
+++ tests/__mocks__/node-notifier.js
@@ -0,0 +1,7 @@
+/**
+ * Mock for the node-notifer package.
+ */
+module.exports = {
+  on: jest.fn(),
+  notify: jest.fn(),
+};
---
+++ tests/webpack.config.ts
@@ -0,0 +1,45 @@
+import WebpackBuildNotifierPlugin from '../src/index';
+import webpack from 'webpack';
+import path from 'path';
+import { Config } from '../src/types';
+
+const getFullPath = (p: string) => path.resolve(__dirname, p);
+
+const getWebpackConfig = (
+  pluginConfig?: Config,
+  result: 'success' | 'warning' | 'error' = 'success',
+  watch: boolean = false
+): webpack.Configuration => ({
+  watch,
+  entry: getFullPath(`${result}.js`),
+  output: {
+    path: getFullPath('assets'),
+    publicPath: '/',
+    filename: `${result}.bundle.js`
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  performance: {
+    hints: 'warning',
+    maxEntrypointSize: result === 'warning' ? 100 : undefined,
+  },
+  plugins: [
+    new WebpackBuildNotifierPlugin({
+      ...pluginConfig,
+      title: 'Build Notification Test',
+      suppressCompileStart: false,
+    })
+  ]
+});
+
+export default getWebpackConfig;
